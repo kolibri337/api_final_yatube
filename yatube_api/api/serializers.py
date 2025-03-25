1f954b0c6c9af0cf5685f8ee394b8f569d9fb8c6
Bondarenko Nikita <bondar3nko.nikit@yandex.ru>
2025-03-25 20:25:40 +0300

    project of the API

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -9,9 +9,7 @@ User = get_user_model()
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        slug_field='username',
-        read_only=True
-    )
+        slug_field='username', read_only=True)
 
     class Meta:
         model = Post
@@ -20,15 +18,11 @@ class PostSerializer(serializers.ModelSerializer):
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='username'
-    )
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+        read_only=True, slug_field='username')
 
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('author', 'post')
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -41,26 +35,19 @@ class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True,
-        default=serializers.CurrentUserDefault()
-    )
+        default=serializers.CurrentUserDefault())
     following = serializers.SlugRelatedField(
-        slug_field='username',
-        queryset=User.objects.all()
-    )
+        slug_field='username', queryset=User.objects.all())
 
     class Meta:
         model = Follow
         fields = '__all__'
         validators = [
             UniqueTogetherValidator(
-                queryset=Follow.objects.all(),
-                fields=('following', 'user'))
+                queryset=Follow.objects.all(), fields=('following', 'user'))
         ]
 
     def validate(self, data):
         if self.context['request'].user == data['following']:
-            raise serializers.ValidationError(
-                'Нельзя подписаться на себя.'
-            )
+            raise serializers.ValidationError('Нельзя подписаться на себя.')
         return data
-      
